3865fb5cb93a56aeffaca333e6fffdbf263c6093
infobip-ci <infobip-ci@infobip.com>
2024-07-10 17:15:00 +0200

    iOS RTC Extensions Release 1.0.4

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         .binaryTarget(
             name: "InfobipRTCExtensions",
             url: "https://rtc.cdn.infobip.com/ios/extensions/1.0.4/InfobipRTCExtensions.zip",
-            checksum: "62a8d521f30b209ae2c65d8e800ef7033c30241f7b1e4c8d77d80afbb5c08be7"
+            checksum: "f59a6c8618cab9bcf70c25058bbe0ac4574695836263f34c8659be47202d9631"
         )
     ]
 )
